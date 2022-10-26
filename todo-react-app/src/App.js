64665328a05e0e0edbb00b115fb1b5b54a953fe4
Yashaswi Anand <yash.kwr17@gmail.com>
2022-10-26 23:55:22 +0530

    update folder

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import Login from './LoginPage'
+import Dashboard from './Dashboard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Todo
+      <Routes>
+        <Route path= "/" element= {<Dashboard/>} />
+        <Route path="/login"  element={<Login/>} />
+      </Routes>
     </div>
   );
 }
